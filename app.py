1c700f559fd5034c5888e74fb8289626b0390b04
oscar-wilson <oscar.wilson8@education.nsw.gov.au>
2025-10-24 10:41:18 +1100

    calc and personal heelo name using flask on website - task1

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return f'<h1>Hello!</h1>'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'<h1>Hello, {name}!</h1>'
+
+@app.route('/calc/<int:num1>/<string:operation>/<int:num2>')
+
+def calculator(num1, operation, num2):
+    op = operation.lower().strip()
+    if op == "add":
+        result = num1 + num2
+    elif op == 'sub':
+        result = num1 - num2
+    elif op == 'mul':
+        result = num1*num2
+    elif op == 'div':
+        result = num1/num2
+    elif op == 'pow':
+        result = num1**num2
+    else: #invalid operation
+        return "<h1>The operations are: add, sub, mul, div, pow</h1>"
+    
+    return f'<h1>{num1} {operation} {num2} = {result}</h1>'
+
+#Nothing past this point bb!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 if __name__ == '__main__':
     app.run(debug=True)
